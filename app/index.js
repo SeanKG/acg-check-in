8fbe7fafa044deb14c0f9dc7f0691edecbd22f3c
Sean Graves <seankgraves@gmail.com>
2019-10-27 09:37:30 -0400

    Fixed roboto again after merge conflicts

--- app/index.js
+++ app/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 import AppNavigator from './router';
 import SignInPage from './pages/SignInPage';
 import GuardLocationPage from './pages/GuardLocationPage';
 
 export default class App extends React.Component {
+    state={
+        isReady: false
+    }
+
+    async componentWillMount() {
+        await Font.loadAsync({
+            'Roboto': require('../node_modules/native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('../node_modules/native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.setState({isReady:true})
+    }  
+
     render() {
+        if (!this.state.isReady) {
+            return null;
+        }
         return <AppNavigator />
     };
 }
