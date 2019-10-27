e3e7f777e04817ff46edcb5f483c825398ed1468
husshegz <husshegazy@gmail.com>
2019-10-27 09:56:11 -0400

    icons

---
+++ app/icons/LogoTextFull.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { G, Path, Ellipse, Circle, Text } from 'react-native-svg';
+
+
+export default {
+    LogoNoText: {
+        viewBox: "0 0 208 131",
+        svg: <G id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <G id="Logo_Text" transform="translate(1.000000, -4.000000)">
+                <Path d="M116.5,4.70886703 C104.519432,1.77877075 87.0620329,5.12745221 64.1278027,14.7549114 C41.1935725,24.3823706 20.4843049,37.3585112 2,53.6833333" id="Path-2" stroke="#53565A" stroke-width="4" transform="translate(59.250000, 28.683333) rotate(3.000000) translate(-59.250000, -28.683333) "></Path>
+                <Path d="M205,107.553788 C194.641256,105.971536 179.547085,107.779824 159.717489,112.978652 C139.887892,118.17748 121.982063,125.184596 106,134" id="Path-2" stroke="#53565A" stroke-width="4" transform="translate(155.500000, 120.500000) rotate(182.000000) translate(-155.500000, -120.500000) "></Path>
+                <Ellipse id="Oval" fill="#ED8B00" cx="109" cy="68.9850041" rx="65" ry="64.9850041"></Ellipse>
+                <Circle id="Oval-2" fill="#FFFFFF" cx="68" cy="69" r="25"></Circle>
+            </G>
+        </G>
+    },
+    LogoTextFull: {
+        viewBox: "0 0 217 171",
+        svg: <G id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <G id="Logo_Text" transform="translate(1.000000, -4.000000)">
+                <Path d="M116.5,4.70886703 C104.519432,1.77877075 87.0620329,5.12745221 64.1278027,14.7549114 C41.1935725,24.3823706 20.4843049,37.3585112 2,53.6833333" id="Path-2" stroke="#53565A" stroke-width="4" transform="translate(59.250000, 28.683333) rotate(3.000000) translate(-59.250000, -28.683333) "></Path>
+                <Path d="M205,107.553788 C194.641256,105.971536 179.547085,107.779824 159.717489,112.978652 C139.887892,118.17748 121.982063,125.184596 106,134" id="Path-2" stroke="#53565A" stroke-width="4" transform="translate(155.500000, 120.500000) rotate(182.000000) translate(-155.500000, -120.500000) "></Path>
+                <Ellipse id="Oval" fill="#ED8B00" cx="109" cy="68.9850041" rx="65" ry="64.9850041"></Ellipse>
+                <Circle id="Oval-2" fill="#FFFFFF" cx="68" cy="69" r="25"></Circle>
+            </G>
+        </G>
+    },
+    Simplified: {
+        viewBox: "0 0 131 130",
+        svg: <G id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <G id="Logo_Text">
+                <Ellipse id="Oval" fill="#ED8B00" cx="66" cy="64.9850041" rx="65" ry="64.9850041"></Ellipse>
+                <Circle id="Oval-2" fill="#FFFFFF" cx="25" cy="65" r="25"></Circle>
+            </G>
+        </G>
+    }
+
+}
